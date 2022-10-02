a7cdba91062b9183969612a49b93ec5c052f32f5
AdamLebo <alieberman10@aol.com>
2022-10-02 06:45:40 -0400

    added prompts and alerts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var userInput = window.prompt("Please select a password length between 8 and 128 characters")
+  var length = parseInt(userInput)
+  if (isNaN(length)) {
+    window.alert("Please choose a number")
+    return
+  }
+  if (8 > length || length > 128) {
+    window.alert("Password does not meet length criteria")
+    return
+  }
+
+  var useNumbers = window.confirm("Would you like your password to contain numbers?")
+  var useLower = window.confirm("Would you like your password to contain lowercase charatcers?")
+  var useUpper = window.confirm("Would you like your password to contain uppercase characters?")
+  var useSpecial = window.confirm("Would you like your password to contain special characters?")
+
+  
+}
 
 // Write password to the #password input
 function writePassword() {
